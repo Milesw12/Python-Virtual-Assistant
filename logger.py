b6660fd6c194d9b7f241523282f11930304fc700
milesw12 <mileswootton01@hotmail.com>
2021-05-10 16:58:28 +0100

    added more responses

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 def LogOutput():
     import logging
+    logging.basicConfig()
     logger = logging.getLogger('assistant')
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(filename='assistant.log', encoding='utf-8', mode='w')
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import speech_recognition as sr
 import pyttsx3
 import pyaudio
 import random
-from logger import *
+from logger import LogOutput
+import datetime
+import calendar
+import getpass
 
 
 engine = pyttsx3.init()
@@ -62,7 +65,7 @@ def greetings(text):
     return ''
 
 def wakeWord(text):
-    WAKE_WORD = ['Computer']
+    WAKE_WORD = ('computer')
 
     text = text.lower()
 
@@ -70,6 +73,21 @@ def wakeWord(text):
         if phrase in text:
             return True
     return False
+
+
+def getDate():
+    now = datetime.datetime.now()
+    my_date = datetime.datetime.today()
+    weekday = calendar.day_name[my_date.weekday()]
+    monthNum = now.month
+    dayNum = now.day
+
+    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                   'November', 'December']
+
+    ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th','14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st']
+
+    return 'Today is ' + weekday + ', ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1]
 
 
 while True:
@@ -81,6 +99,15 @@ while True:
             responses = responses + greetings(text)
         except Exception as e:
             responses = responses + ''
+
+    if 'date' in text:
+        get_date = getDate()
+        responses = responses + ' ' + get_date
+
+    if 'what is my name' or 'who am i' in text:
+        user = getpass.getuser()
+        responses = responses + ' ' + user
+
     else:
         responses = responses + "I'm sorry i can't do that yet" 
 
